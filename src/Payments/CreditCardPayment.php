d131b8a485a65e64f7ea172fd813a863b3a466c7
Ian Rodrigues <ianrodriguesbr@gmail.com>
2016-08-26 17:00:17 -0300

    changed to get integer amount

--- src/Payments/CreditCardPayment.php
+++ src/Payments/CreditCardPayment.php
@@ -19,7 +19,7 @@ class CreditCardPayment extends Payment implements Arrayable
 
     protected $brandPatterns = [
         'Visa'            => '/^4[0-9]{12}(?:[0-9]{3})?$/',
-        'MasterCard'      => '/^5[1-5][0-9]{14}$/',
+        'Master'          => '/^5[1-5][0-9]{14}$/',
         'JCB'             => '/^(?:2131|1800|35\d{3})\d{11}$/',
         'Discover'        => '/^6(?:011|5[0-9][0-9])[0-9]{12}$/',
         'DinersClub'      => '/^3(?:0[0-5]|[68][0-9])[0-9]{11}$/',
@@ -88,7 +88,7 @@ class CreditCardPayment extends Payment implements Arrayable
     {
         return [
             'Type' => $this->paymentType,
-            'Amount' => $this->amount,
+            'Amount' => $this->getIntegerAmount(),
             'Installments' => $this->installments,
             'CreditCard' => [
                 'CardNumber' => $this->cardNumber,
